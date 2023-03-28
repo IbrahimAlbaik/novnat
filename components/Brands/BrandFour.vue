4673a5f0a2701227568ec16605920b8b0e58c29a
Asmaa Albaik <asmaa.baik@gmail.com>
2023-03-29 01:22:04 +0200

    update partners section

--- components/Brands/BrandFour.vue
+++ components/Brands/BrandFour.vue
@@ -5,37 +5,57 @@
         <div class="col-xl-12">
           <div class="brand-one-carousel owl-carousel">
             <no-ssr>
-              <carousel :autoplay="true" :dots="false" :nav="false" :responsive="{0: {items: 1},640: {items: 2},992: {items: 3},1024: {items: 5}}">
+              <carousel
+                :autoplay="true"
+                :dots="false"
+                :nav="false"
+                :responsive="{
+                  0: { items: 1 },
+                  640: { items: 2 },
+                  992: { items: 3 },
+                  1024: { items: 5 },
+                }"
+              >
                 <div class="single_brand_item">
-                  <a href="#"><img src="/assets/images/resources/brand-1-1.png" alt="brand"></a>
+                  <a href="https://www.gbslepgrowthhub.co.uk/programmes/amtecaa"
+                    ><img
+                      src="/assets/images/partners/amtecaa.png"
+                      alt="brand"
+                  /></a>
                 </div>
                 <div class="single_brand_item">
-                  <a href="#"><img src="/assets/images/resources/brand-1-2.png" alt="brand"></a>
+                  <a href="https://www.birmingham.ac.uk/research/energy/climate-innovation-platform.aspx"
+                    ><img
+                      src="/assets/images/partners/climate.png"
+                      alt="brand"
+                  /></a>
                 </div>
                 <div class="single_brand_item">
-                  <a href="#"><img src="/assets/images/resources/brand-1-3.png" alt="brand"></a>
+                  <a href="https://www.birmingham.ac.uk/index.aspx"
+                    >
+                    <!-- incubated by: -->
+                    <img
+                      src="/assets/images/partners/university.jpg"
+                      alt="brand"
+                  /></a>
                 </div>
                 <div class="single_brand_item">
-                  <a href="#"><img src="/assets/images/resources/brand-1-4.png" alt="brand"></a>
+                  <a href="https://intranet.birmingham.ac.uk/as/employability/b-enterprising/uobelevate/index.aspx"
+                    >
+                    <!-- incubated by: -->
+                    <img
+                      src="/assets/images/partners/UOB.png"
+                      alt="brand"
+                  /></a>
                 </div>
                 <div class="single_brand_item">
-                  <a href="#"><img src="/assets/images/resources/brand-1-5.png" alt="brand"></a>
+                  <a href="https://ec.europa.eu/regional_policy/funding/erdf_en"
+                    ><img
+                      src="/assets/images/partners/union.jpg"
+                      alt="brand"
+                  /></a>
                 </div>
-                <div class="single_brand_item">
-                  <a href="#"><img src="/assets/images/resources/brand-1-1.png" alt="brand"></a>
-                </div>
-                <div class="single_brand_item">
-                  <a href="#"><img src="/assets/images/resources/brand-1-2.png" alt="brand"></a>
-                </div>
-                <div class="single_brand_item">
-                  <a href="#"><img src="/assets/images/resources/brand-1-3.png" alt="brand"></a>
-                </div>
-                <div class="single_brand_item">
-                  <a href="#"><img src="/assets/images/resources/brand-1-4.png" alt="brand"></a>
-                </div>
-                <div class="single_brand_item">
-                  <a href="#"><img src="/assets/images/resources/brand-1-5.png" alt="brand"></a>
-                </div>
+
               </carousel>
             </no-ssr>
           </div>
@@ -46,11 +66,17 @@
 </template>
 
 <script>
-    export default {
-        name: "BrandFour"
-    }
+export default {
+  name: "BrandFour",
+};
 </script>
 
 <style scoped>
-
+.single_brand_item {
+  margin: 0 0.5rem;
+}
+.single_brand_item a img {
+  height: 90px;
+  object-fit: fill;
+}
 </style>
